492e8870eb5129443dcc4fbe1605be6bcb9af379
ernesto <ernestoballono@gmail.com>
2019-05-12 18:59:59 -0400

    splice algorithm finish

--- problems/05-substring/05-substring.js
+++ problems/05-substring/05-substring.js
@@ -3,19 +3,33 @@ Define function substring(someStr, startIndex, endIndex)
 Return the substring contained between startIndex (inclusive) and endIndex (exclusive).
 If incorrect input is entered, throw an exception stating the input was incorrect.
 For example substring('hello', 0, 3) should give 'hel' */
+
 function substring(someStr, startIndex, endIndex) {
+
+    let start = startIndex || 0;
+    let len = someStr.length;
+    let end = endIndex || len-1 ;
     let newArr = [];
-    let arr = someStr;
-    for(let x = 0; x < arr.length;x++){
-        if(startIndex<=x && x<endIndex){
-            newArr.push(arr[x]);
+
+    if (start<0) start += len;
+    if (end<0) end += len;
+
+    start = Math.max(Math.min(start,len),0);// -len+1 < start < len-1
+    end = Math.min(Math.max(end,0),len);//      -len+1 < end < len-1
+
+    for(let x = 0; x < len;x++){
+        if(start <= x && x < end){
+            newArr.push(someStr[x]);
         }
     }
+    
     return newArr.join("");
 
 }
 console.log(substring("hello",0,3));// hel
 console.log(substring("hello",2,3));//l
 console.log(substring("ernesto",2,3));//n
-console.log(substring("hello",2,30));//hello
-console.log(substring("hello",-2,3));//hel
+console.log(substring("hello",2,30));//llo
+console.log(substring("hello",-2,3));//(empty string)
+console.log(substring("hello",0,5));//hello
+console.log("hello".slice(2,"2"));//hel
--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,57 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
-function spliceElement(someArr, index) {
-  
+function spliceElement(someArr, start , deleteCount) {
+    const max = Math.max,
+          min = Math.min,
+          len = someArr.length;
+          
+    let element,//new element to insert
+        k=0,//index counter in somearr
+        new_len,//new arr len
+        result = [],
+        shift_count,//index counter in the new array
+        toRemove,//count ofitem to remove
+        toInsert = max(arguments.length-3,0);//count of item to insert
+
+    start = start || 0;
+    if(start < 0){
+        start += len;
+    }
+    start = max(min(start,len),0);
+    toRemove = max(min(typeof deleteCount === "number" ? deleteCount : len,len-start),0) || 0 ;
+    
+    new_len = len-1 + toInsert - deleteCount;
+    result.length = new_len;
+    while(k < start){
+        element = someArr[k];
+        if (element !== undefined){
+            result[k] = element;
+        }
+        k+=1;
+    }
+    shift_count = k;
+    while(toRemove >  0){
+            k++;
+            toRemove--;
+    }
+    
+    let counter = 0;
+    while (toInsert > 0){
+        result[shift_count]= arguments[3 + counter];
+        shift_count++;
+        counter++;
+        toInsert--;   
+    }  
+    while(k < someArr.length){
+        element = someArr[k];
+        if (element !== undefined){
+            result[shift_count] = element;
+        }
+        shift_count+=1;
+        k++;
+    }
+    return result;
 }
+console.log(spliceElement('heo',2,0,'l','l'));//hello
+console.log(spliceElement('1234467',4,1,'5'));//1234567
